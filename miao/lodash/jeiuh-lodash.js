a530a56dccddeed2bb40e9e6aa894cd942f2e7f0
Jeiuh <52284947+Jeiuh@users.noreply.github.com>
2020-12-08 20:38:49 +0800

    Update jeiuh-lodash.js

--- miao/lodash/jeiuh-lodash.js
+++ miao/lodash/jeiuh-lodash.js
@@ -4,6 +4,14 @@ var jeiuh = {
         if (ary) {
             return ary.splice(n)
         }
+    },
+
+    dropright: function(ary,n=1) {
+        var ary1 = new Array()
+        for (let i = 1; i <= ary.length-n; i++) {
+            ary1.push(i)
+        }
+        return ary1
     }
 
 }
